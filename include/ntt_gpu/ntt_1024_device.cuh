17698ad9a009b80f2dbb80807d996bb2da2bcf8f
nindanaoto <matsuoka.kotaro@gmail.com>
2023-04-12 16:13:20 +0000

    bit refactored to rm magic numbers

--- include/ntt_gpu/ntt_1024_device.cuh
+++ include/ntt_gpu/ntt_1024_device.cuh
@@ -34,6 +34,11 @@ __constant__ FFP con_1024_twd_inv[1024];
 __constant__ FFP con_1024_twd_sqrt[1024];
 __constant__ FFP con_1024_twd_sqrt_inv[1024];
 
+constexpr uint numdataperthreadbit = 3;
+constexpr uint dimoverthreadbit = 10 - numdataperthreadbit;
+
+constexpr uint numdataperthread = 1<<numdataperthreadbit;
+
 __device__ inline
 void NTT1024Core(FFP* r,
                  FFP* s,
@@ -41,43 +46,41 @@ void NTT1024Core(FFP* r,
                  const uint3& t3d) {
   FFP *ptr = nullptr;
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    //r[i] *= twd_sqrt[(i << 7) | t1d]; // mult twiddle sqrt
-    r[i] *= con_1024_twd_sqrt[(i << 7) | t1d]; // mult twiddle sqrt
+  for (int i = 0; i < numdataperthread; i ++)
+    r[i] *= con_1024_twd_sqrt[(i << dimoverthreadbit) | t1d]; // mult twiddle sqrt
   NTT8(r);
   NTT8x2Lsh(r, t3d.z); // if (t1d >= 64) NTT8x2<1>(r);
   ptr = &s[(t3d.y << 7) | (t3d.z << 6) | (t3d.x << 2)];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[(i >> 2 << 5) | (i & 0x3)] = r[i];
   __syncthreads();
 
   ptr = &s[(t3d.z << 9) | (t3d.y << 3) | t3d.x];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] = ptr[i << 6];
-  NTT2(r);
-  NTT2(r + 2);
-  NTT2(r + 4);
-  NTT2(r + 6);
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread/2; i ++)
+    NTT2(r + 2*i);
+  #pragma unroll
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[i << 6] = r[i];
   __syncthreads();
 
   ptr = &s[t1d];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    r[i] = ptr[i << 7] * con_1024_twd[(i << 7) | t1d]; // mult twiddle
+  for (int i = 0; i < numdataperthread; i ++)
+    r[i] = ptr[i << 7] * con_1024_twd[(i << dimoverthreadbit) | t1d]; // mult twiddle
   NTT8(r);
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[i << 7] = r[i];
   __syncthreads();
 
   ptr = &s[(t1d >> 2 << 5) | (t3d.x & 0x3)];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] = ptr[i << 2];
   NTT8x8Lsh(r, t1d >> 4); // less divergence if put here!
   NTT8(r);
@@ -94,41 +97,40 @@ void NTTInv1024Core(FFP* r,
   NTTInv8x2Lsh(r, t3d.z); // if (t1d >= 64) NTT8x2<1>(r);
   ptr = &s[(t3d.y << 7) | (t3d.z << 6) | (t3d.x << 2)];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[(i >> 2 << 5) | (i & 0x3)] = r[i];
   __syncthreads();
 
   ptr = &s[(t3d.z << 9) | (t3d.y << 3) | t3d.x];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] = ptr[i << 6];
-  NTT2(r);
-  NTT2(r + 2);
-  NTT2(r + 4);
-  NTT2(r + 6);
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread/2; i ++)
+    NTT2(r+2*i);
+  #pragma unroll
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[i << 6] = r[i];
   __syncthreads();
 
   ptr = &s[t1d];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] = ptr[i << 7] * con_1024_twd_inv[(i << 7) | t1d]; // mult twiddle
   NTTInv8(r);
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     ptr[i << 7] = r[i];
   __syncthreads();
 
   ptr = &s[(t1d >> 2 << 5) | (t3d.x & 0x3)];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] = ptr[i << 2];
   NTTInv8x8Lsh(r, t1d >> 4); // less divergence if put here!
   NTTInv8(r);
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
+  for (int i = 0; i < numdataperthread; i ++)
     r[i] *= con_1024_twd_sqrt_inv[(i << 7) | t1d]; // mult twiddle sqrt
 }
 
@@ -141,16 +143,16 @@ void NTT1024(FFP* out,
   const uint32_t t1d = ThisThreadRankInBlock() - leading_thread;
   uint3 t3d;
   Index3DFrom1D<8, 8, 2>(t3d, t1d);
-  register FFP r[8];
+  register FFP r[numdataperthread];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    r[i] = FFP((T)in[(i << 7) | t1d]);
+  for (int i = 0; i < numdataperthread; i ++)
+    r[i] = FFP((T)in[(i << dimoverthreadbit) | t1d]);
   __syncthreads();
   NTT1024Core(r, temp_shared, t1d, t3d);
   __syncthreads();
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    out[(i << 7) | t1d] = r[i];
+  for (int i = 0; i < numdataperthread; i ++)
+    out[(i << dimoverthreadbit) | t1d] = r[i];
 }
 
 template <typename T>
@@ -162,18 +164,18 @@ void NTTInv1024(T* out,
   const uint32_t t1d = ThisThreadRankInBlock() - leading_thread;
   uint3 t3d;
   Index3DFrom1D<8, 8, 2>(t3d, t1d);
-  register FFP r[8];
+  register FFP r[numdataperthread];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    r[i] = in[(i << 7) | t1d];
+  for (int i = 0; i < numdataperthread; i ++)
+    r[i] = in[(i << dimoverthreadbit) | t1d];
   __syncthreads();
   NTTInv1024Core(r, temp_shared, t1d, t3d);
   __syncthreads();
   // mod 2^32 specifically
   constexpr uint64_t med = FFP::kModulus() / 2;
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    out[(i << 7) | t1d] = (T)r[i].val() - (r[i].val() > med);
+  for (int i = 0; i < numdataperthread; i ++)
+    out[(i << dimoverthreadbit) | t1d] = (T)r[i].val() - (r[i].val() > med);
 }
 
 template <typename T>
@@ -185,18 +187,18 @@ void NTTInv1024Add(T* out,
   const uint32_t t1d = ThisThreadRankInBlock() - leading_thread;
   uint3 t3d;
   Index3DFrom1D<8, 8, 2>(t3d, t1d);
-  register FFP r[8];
+  register FFP r[numdataperthread];
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    r[i] = in[(i << 7) | t1d];
+  for (int i = 0; i < numdataperthread; i ++)
+    r[i] = in[(i << dimoverthreadbit) | t1d];
   __syncthreads();
   NTTInv1024Core(r, temp_shared, t1d, t3d);
   __syncthreads();
   // mod 2^32 specifically
   constexpr uint64_t med = FFP::kModulus() / 2;
   #pragma unroll
-  for (int i = 0; i < 8; i ++)
-    out[(i << 7) | t1d] += T(r[i].val() - (r[i].val() >= med));
+  for (int i = 0; i < numdataperthread; i ++)
+    out[(i << dimoverthreadbit) | t1d] += T(r[i].val() - (r[i].val() >= med));
 }
 
 } // namespace cufhe
